ce4c33fdfabff409fa63691ff82df01a890e9ee0
Sanjith Chockan <sanjith.chockan@gmail.com>
2022-03-07 23:55:18 -0600

    Setup CPU process to communicate with Memory process

--- CPU.java
+++ CPU.java
@@ -1,5 +1,37 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class CPU {
-    public static void Main(String [] args) {
-        
+    public static void main(String [] args) {
+        try {
+            Runtime rt = Runtime.getRuntime();
+
+            Process proc = rt.exec("java Memory");
+            
+            InputStream is = proc.getInputStream();
+            OutputStream os = proc.getOutputStream();
+
+            PrintWriter pw = new PrintWriter(os);
+            pw.println(50);
+            System.out.println("After sending 50");
+            pw.flush();
+            System.out.println("After flushing");
+
+            Scanner sc = new Scanner(is);
+            System.out.println("Before loading address");
+            int address = sc.nextInt();
+            System.out.println("After loading address");
+            System.out.print("Address: ");
+            System.out.println(address);
+            
+            //int exitVal = proc.exitValue();
+
+            //System.out.println("Process exited: " + exitVal);
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
     }
 }
--- Memory.java
+++ Memory.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Memory {
     public static void main(String [] args) {
+        int [] memory = new int [2000];
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x);
         
     }
 }
