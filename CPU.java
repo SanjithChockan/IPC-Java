8f4d268bc0822760fe400376dfbb42847c116eec
Sanjith Chockan <sanjith.chockan@gmail.com>
2022-03-08 02:29:32 -0600

    Issues with IPC. Works well the first iteration, but scanner doesn't find anything after.

--- CPU.java
+++ CPU.java
@@ -4,34 +4,61 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class CPU {
+
+    static int PC = 0;
+    static int SP = 0;
+    static int IR = 0;
+    static int AC = 0;
+    static int X, Y = 0;
     public static void main(String [] args) {
         try {
             Runtime rt = Runtime.getRuntime();
-
             Process proc = rt.exec("java Memory");
-            
-            InputStream is = proc.getInputStream();
-            OutputStream os = proc.getOutputStream();
+            InputStream is;
+            OutputStream os;
 
-            PrintWriter pw = new PrintWriter(os);
-            pw.println(50);
-            System.out.println("After sending 50");
-            pw.flush();
-            System.out.println("After flushing");
+            while (PC <= 8) {
+                is = proc.getInputStream();
+                os = proc.getOutputStream();
 
-            Scanner sc = new Scanner(is);
-            System.out.println("Before loading address");
-            int address = sc.nextInt();
-            System.out.println("After loading address");
-            System.out.print("Address: ");
-            System.out.println(address);
+                PrintWriter pw = new PrintWriter(os);
+                System.out.println("PC: " + PC);
+                pw.println(PC);
+                pw.flush();
+
+                Scanner sc = new Scanner(is);
+                System.out.println("Has next: " + sc.hasNext());
+                //int value = sc.nextInt();
+                //execInstruction(value, proc);
+                //System.out.println("Value at address: " + "inf");
+                PC++;
+            }
             
             //int exitVal = proc.exitValue();
-
             //System.out.println("Process exited: " + exitVal);
 
         } catch (Throwable t) {
             t.printStackTrace();
         }
+    }
+
+    static void execInstruction(int value, Process proc) {
+        switch(value) {
+            case 50:
+                try {
+                    proc.waitFor();
+                    int exit = proc.exitValue();
+                    System.out.println("Process exited: " + exit);
+                    System.exit(0);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            default:
+                System.out.println(0);
+                PC++;
+        }
+
     }
 }
