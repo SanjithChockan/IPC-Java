89caa899c08b88ea5fc1ecc2c9d1644647e1b265
Sanjith Chockan <sanjith.chockan@gmail.com>
2022-03-08 16:30:15 -0600

    Test case to add 3 random generated numbers work! Added few more cases for instructions.

--- CPU.java
+++ CPU.java
@@ -1,11 +1,12 @@
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.Random;
 import java.util.Scanner;
 
 public class CPU {
 
-    static int PC = 0;
+    static int PC = -1;
     static int SP = 0;
     static int IR = 0;
     static int AC = 0;
@@ -14,20 +15,12 @@ public class CPU {
         try {
             Runtime rt = Runtime.getRuntime();
             Process proc = rt.exec("java Memory");
-            InputStream is = proc.getInputStream();
-            OutputStream os = proc.getOutputStream();
-            Scanner sc = new Scanner(is);
-
+            
             while (PC <= 9) {
+                incrementPC();
                 
-                PrintWriter pw = new PrintWriter(os);
-                System.out.println("PC: " + PC);
-                pw.println(PC);
-                pw.flush();
-
-                int value = sc.nextInt();
-                System.out.println("Value and PC: " + value + " and " + PC);
-                execInstruction(value, proc);
+                IR = fetch(proc);
+                execInstruction(IR, proc);
             }
             
             //int exitVal = proc.exitValue();
@@ -38,17 +31,81 @@ public class CPU {
         }
     }
 
-    static void execInstruction(int value, Process proc) {
-        switch(value) {
+    static void incrementPC() {
+        PC++;
+    }
+
+    static int fetch(Process proc) {
+        InputStream is = proc.getInputStream();
+        OutputStream os = proc.getOutputStream();
+        Scanner sc = new Scanner(is);
+
+        PrintWriter pw = new PrintWriter(os);
+        pw.println("r " + PC);
+        pw.flush();
+        return sc.nextInt();
+    }
+
+    static void execInstruction(int instruction, Process proc) {
+
+
+        switch(instruction) {
+            // load value into AC
+            case 1:
+                incrementPC();
+                AC = fetch(proc);
+                break;
+
+            // load the value at the address into AC
+            case 2:
+                incrementPC();
+                int tempAddress = fetch(proc);
+                int temp = PC;
+                PC = tempAddress;
+                AC = fetch(proc);
+                PC = temp;
+
+            // Generate random number (1 - 100) and store it in AC
+            case 8:
+                Random rand = new Random();
+                int randomNum = rand.nextInt((99) + 1) + 1;
+                AC = randomNum;
+                break;
+            
+            // put port: If port == 1, print AC as an int; if port == 2, print AC as a char
+            case 9:
+                incrementPC();
+                int port = fetch(proc);
+                if (port == 1) {
+                    System.out.println(AC);
+                }
+                else if (port == 2) {
+                    System.out.println((char)AC);
+                }
+
+            // Add the value in X to the AC
+            case 10:
+                AC += X;
+                break;
+            // Add the value in Y to the AC
+            case 11:
+                AC += Y;
+                break;
+            // Move value from AC to X
+            case 14:
+                X = AC;
+                break;
+            // Move value from AC to Y
+            case 16:
+                Y = AC;
+                break;
             case 50:
             // need to implement proc.waitFor to first close the other program
-                System.out.println("Value: " + value);
-                System.out.println("Exiting!");
                 System.exit(0);
+                break;
 
             default:
-                System.out.println("Value: " + value);
-                PC++;
+                ;
         }
 
     }
--- Memory.java
+++ Memory.java
@@ -1,14 +1,40 @@
 import java.util.Scanner;
 
 public class Memory {
+
+    static int [] memory = new int [] {8, 14, 8, 16, 8, 10, 11, 9, 1, 50};
     public static void main(String [] args) {
-        int [] memory = new int [] {8, 14, 8, 16, 8, 10, 11, 9, 1, 50};
 
         while (true) {
             Scanner sc = new Scanner(System.in);
-            int x = sc.nextInt();
-            System.out.println(memory[x]);
+            String x = sc.nextLine();
+            String [] cmd = x.split(" ");
+            char op = cmd[0].charAt(0);
+            int address = Integer.parseInt(cmd[1]);
+
+            switch(op) {
+                case 'r':
+                    int value = read(address);
+                    System.out.println(value);
+                    break;
+                case 'w':
+                    int data = Integer.parseInt(cmd[2]);
+                    write(address, data);
+                    break;
+                default:
+                    System.out.println("Error");
+            }
         }
         
+    }
+
+    // returns the value at the address
+    static int read(int address) {
+        return memory[address];
+    }
+
+    // writes the data to the address
+    static void write(int address, int data) {
+        memory[address] = data;
     }
 }
